8fa7a8133eba1f729c70fdd0f68cfdecd7bd06e3
jimohsegun1 <jimohsegunj@gmail.com>
2024-04-23 11:07:56 +0100

    little change

--- frontend/src/components/CategoryForm.jsx
+++ frontend/src/components/CategoryForm.jsx
@@ -10,7 +10,7 @@ const CategoryForm = ({
         <form onSubmit={handleSubmit} className="space-y-3">
           <input
             type="text"
-            className="py-3 px-4 border rounded-lg w-full"
+            className="bg-black py-3 px-4 border rounded-lg w-full"
             placeholder="Write category name"
             value={value}
             onChange={(e) => setValue(e.target.value)}

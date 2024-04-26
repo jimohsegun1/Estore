cc11047189687741e6b849082f85137d74f132c9
jimohsegun1 <jimohsegunj@gmail.com>
2024-04-26 15:57:11 +0100

    shop ui, productCard, search, filtering

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -23,6 +23,7 @@ import Favorites from "./pages/Products/Favorites.jsx";
 import ProductDetails from "./pages/Products/ProductDetails.jsx";
 import Home from "./pages/Home.jsx";
 import Cart from "./pages/Cart.jsx";
+import Shop from "./pages/shop.jsx";
 
 
 const router = createBrowserRouter(
@@ -34,6 +35,7 @@ const router = createBrowserRouter(
       <Route path="/favorite" element={<Favorites />} />
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
+      <Route path="/shop" element={<Shop />} />
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile/>} />

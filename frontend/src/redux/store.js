3cc187149d9e13d32ba8d41ad1afbb00acecfb42
jimohsegun1 <jimohsegunj@gmail.com>
2024-04-26 14:33:28 +0100

    shop redux setup

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -5,6 +5,7 @@ import authReducer from "./features/auth/authSlice";
 import favoritesReducer from "../redux/features/favorites/favoriteSlice";
 import { getFavoritesFromLocalStorage } from "../Utils/localStorage";
 import cartSliceReducer from "../redux/features/cart/cartSlice";
+import shopReducer from "../redux/features/shop/shopSlice";
 
 const initialFavorites = getFavoritesFromLocalStorage() || [];
 
@@ -14,6 +15,7 @@ const store = configureStore({
     auth: authReducer,
     favorites: favoritesReducer,
     cart: cartSliceReducer,
+    shop: shopReducer,
   },
 
   preloadedState: {

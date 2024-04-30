710d2ba4a05f6e0744ce7db3230723e747afa0d4
jimohsegun1 <jimohsegunj@gmail.com>
2024-04-30 01:41:30 +0100

    implement order ui, place order, shipping,userorder etc

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -24,7 +24,12 @@ import ProductDetails from "./pages/Products/ProductDetails.jsx";
 import Home from "./pages/Home.jsx";
 import Cart from "./pages/Cart.jsx";
 import Shop from "./pages/shop.jsx";
-
+import Shipping from "./pages/Orders/Shipping.jsx";
+import Order from "./pages/Orders/Order.jsx";
+import PlaceOrder from "./pages/Orders/PlaceOrder.jsx";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import UserOrder from "./pages/User/UserOrder.jsx";
+import OrderList from "./pages/Admin/OrderList.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,26 +41,32 @@ const router = createBrowserRouter(
       <Route path="/product/:id" element={<ProductDetails />} />
       <Route path="/cart" element={<Cart />} />
       <Route path="/shop" element={<Shop />} />
+      <Route path="/user-orders" element={<UserOrder />} />
 
       <Route path="" element={<PrivateRoute />}>
-        <Route path="/profile" element={<Profile/>} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/order/:id" element={<Order />} />
       </Route>
 
       {/* Admin Routes */}
       <Route path="/admin" element={<AdminRoute />}>
-        <Route path="userlist" element={<UserList/>} />
-        <Route path="categorylist" element={<CategoryList/>} />
-        <Route path="productlist" element={<ProductList/>} />
-        <Route path="allProductslist" element={<AllProducts/>} />
-        <Route path="product/update/:_id" element={<ProductUpdate/>} />
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allProductslist" element={<AllProducts />} />
+        <Route path="orderlist" element={<OrderList />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
       </Route>
-
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+    <PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
   </Provider>
 );
